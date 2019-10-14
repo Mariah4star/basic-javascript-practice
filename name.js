d22851968af3c9b4483151458430e2dc6e70630c
Mariah4star <mya.foerster@gmail.com>
2019-10-14 11:18:53 -0600

    get basis javascript to work

--- name.js
+++ name.js
@@ -1,1 +1,34 @@
+import {films} from "./assets/films.js"
+import {people} from "./assets/people.js"
+
 console.log('I am javascipt running in your page')
+
+let mainArea = document.querySelector('main')
+
+films.forEach(function(film) {
+    let filmDiv = document.createElement('div')
+    let filmTitle = document.createElement('h1')
+    let filmCrawl = document.createElement('p')
+
+    filmTitle.textContent = film.title
+    filmCrawl.textContent = film.opening_crawl
+
+    filmDiv.appendChild(filmTitle)
+    filmDiv.appendChild(filmCrawl)
+
+    mainArea.appendChild(filmDiv)
+  });
+  
+people.forEach((person) => {
+    let personDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let gender = document.createElement('p')
+
+    name.textContent = person.name
+    gender.textContent = person.gender
+
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+
+    mainArea.appendChild(person)
+} );
